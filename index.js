ce1e89be28b8131da49c6d422e0dd8012deebf94
jjb1222 <jjb1222@protonmail.com>
2020-04-13 20:24:23 +0100

    fixing log type

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const fs = require('fs');
+// const fs = require('fs');
 
 // Express Controllers
 const estimator = require('./src/app/estimator');
